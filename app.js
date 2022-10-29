eb9dadf5d698328c9da76b8520c1f40171510d27
Viktor <viktor.shchokin.a@gmail.com>
2022-10-29 19:56:26 +0300

    refactor: add general class for list items, rule 3.4

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var completedTasksHolder = document.getElementById('completed-tasks'); //complet
 //New task list item
 var createNewTaskElement = function (taskString) {
   var listItem = document.createElement('li');
+  listItem.classList.add('task-item');
 
   //input (checkbox)
   var checkBox = document.createElement('input'); //checkbx
@@ -30,12 +31,13 @@ var createNewTaskElement = function (taskString) {
   var deleteButtonImg = document.createElement('img'); //delete button image
 
   label.innerText = taskString;
-  label.className = 'task';
+  label.classList.add('task');
 
   //Each elements, needs appending
   checkBox.type = 'checkbox';
   editInput.type = 'text';
-  editInput.className = 'task';
+  editInput.classList.add('task');
+  editInput.classList.add('task-input');
 
   editButton.innerText = 'Edit'; //innerText encodes special characters, HTML does not.
   editButton.className = 'edit';
